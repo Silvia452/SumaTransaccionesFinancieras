09c557c4d226659d813cbef7684982ddada64d59
Silvia Lorero Salor <1004291182alumnos.uc3m.es>
2023-09-17 11:50:56 +0200

    Clases

--- ProcesadorContabilidad.java
+++ ProcesadorContabilidad.java
@@ -1,10 +1,32 @@
-import java.io.IOException;
+import java.io.*;
 
-public abstract class ProcesadorContabilidad implements Runnable {
+public class ProcesadorContabilidad implements Runnable {
     private String archivo;
 
     public ProcesadorContabilidad(String archivo) {
         this.archivo = archivo;
     }
 
+    @Override
+    public void run() {
+        long sumaDepartamento = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                long transaccion = Long.parseLong(linea);
+                sumaDepartamento += transaccion;
+            }
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        //Guardar el resultado en el archivo
+        String resultadoArchivo = archivo + ".res";
+        try (PrintWriter pw = new PrintWriter(new FileWriter(resultadoArchivo))) {
+            pw.println(sumaDepartamento);
+        } catch (IOException e) {
+            //Excepción de escritura
+            e.printStackTrace();
+        }
+    }
 }
